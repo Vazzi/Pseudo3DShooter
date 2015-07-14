437ffd58d6ad6491c1d8d056f2effbd945d8136b
Jakub Vlasak <vlasakjakub@gmail.com>
2015-07-14 13:34:15 +0200

    Indentation

--- src/FontManager.cpp
+++ src/FontManager.cpp
@@ -5,10 +5,10 @@
 FontManager* FontManager::s_pInstance = 0;
 
 const string FontManager::s_letters = ""
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?\"'/\\<>()[]{}"
-            "abcdefghijklmnopqrstuvwxyz_               "
-            "0123456789+-=*:;ÖÅÄå                      "
-            "";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?\"'/\\<>()[]{}"
+"abcdefghijklmnopqrstuvwxyz_               "
+"0123456789+-=*:;ÖÅÄå                      "
+"";
 
 FontManager::FontManager() {
     // empty
@@ -62,31 +62,31 @@ void FontManager::clearFromFontMap(string id) {
 }
 
 SDL_Rect FontManager::sourceRectangle(int character, const FontParams &params) {
-        int xOffset = character % 42;
-        int yOffset = character / 42;
-        FontStruct* font = m_fontMap[params.getId()];
+    int xOffset = character % 42;
+    int yOffset = character / 42;
+    FontStruct* font = m_fontMap[params.getId()];
 
-        SDL_Rect srcRect;
+    SDL_Rect srcRect;
 
-        srcRect.x = xOffset * (font->width + params.getSpacing());
-        srcRect.y = yOffset * font->height;
-        srcRect.w = font->width;
-        srcRect.h = font->height;
+    srcRect.x = xOffset * (font->width + params.getSpacing());
+    srcRect.y = yOffset * font->height;
+    srcRect.w = font->width;
+    srcRect.h = font->height;
 
-        return srcRect;
+    return srcRect;
 }
 
 SDL_Rect FontManager::destinationRectangle(int charIndex, const FontParams &params) {
-        FontStruct* font = m_fontMap[params.getId()];
-        SDL_Rect destRect;
+    FontStruct* font = m_fontMap[params.getId()];
+    SDL_Rect destRect;
 
-        destRect.w = font->width * params.getScale();
-        destRect.h = font->height * params.getScale();
-        destRect.x = params.getX() + charIndex * (font->width + params.getSpacing())
-            * params.getScale();
-        destRect.y = params.getX();
+    destRect.w = font->width * params.getScale();
+    destRect.h = font->height * params.getScale();
+    destRect.x = params.getX() + charIndex * (font->width + params.getSpacing())
+        * params.getScale();
+    destRect.y = params.getX();
 
-        return destRect;
+    return destRect;
 }
 
 
