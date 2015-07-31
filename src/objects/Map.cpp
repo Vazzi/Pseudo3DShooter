bf0b4ed3a2c6c6cc7ab88abbb4c04290a8b942b0
Jakub Vlasak <vlasakjakub@gmail.com>
2015-07-31 19:02:34 +0200

    Map created

---
+++ src/objects/Map.cpp
@@ -0,0 +1,55 @@
+#include "Map.hpp"
+#include <iostream>
+
+Map::Map(const int height, const int width) {
+    m_height = height;
+    m_width = width;
+    m_map = new int[width * height];
+}
+
+Map::~Map() {
+    for (unsigned int i = 0; i < m_walls.size(); i++) {
+        SDL_FreeSurface(m_walls[i]);
+    }
+    m_walls.clear();
+}
+
+void Map::loadMap(std::vector<int> data) {
+    // TODO: load map
+}
+
+bool Map::loadWallBitmap(std::string fileName) {
+    SDL_Surface* pTempSurface = IMG_Load(fileName.c_str());
+    if (pTempSurface == 0) {
+        std::cerr << "File " << fileName << " was not found.\n";
+        return false;
+    }
+    m_walls.push_back(pTempSurface);
+    return true;
+}
+
+SDL_Surface* Map::getWall(const unsigned int x, const unsigned int y) {
+    if (x < m_width && y < m_height) {
+        int wallIndex = m_map[x + (m_width * y)];
+        wallIndex--; // 0 is no object but objects index starts from 0
+        if (wallIndex > 0 && wallIndex < m_walls.size()) {
+            return m_walls[wallIndex];
+        }
+    }
+    return NULL;
+}
+
+bool Map::isEmpty(const unsigned int x, const unsigned int y) {
+    if (x < m_width && y < m_height) {
+        return !(m_map[x + (m_width * y)]);
+    }
+    return true;
+}
+
+// TODO: What is wall and what is object?
+bool Map::isWall(const unsigned int x, const unsigned int y) {
+    if (x < m_width && y < m_height) {
+        return m_map[x + (m_width * y)];
+    }
+    return false;
+}
---
+++ src/objects/Map.hpp
@@ -0,0 +1,26 @@
+#ifndef __MAP__
+#define __MAP__
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <vector>
+
+class Map {
+    public:
+        Map(const int width , const int height);
+        ~Map ();
+        void loadMap(std::vector<int> data);
+        // TODO: Rename wall to objects
+        bool loadWallBitmap(const std::string fileName);
+        SDL_Surface* getWall(const unsigned int x, const unsigned int y);
+        bool isEmpty(const unsigned int x, const unsigned int y);
+        bool isWall(const unsigned int x, const unsigned int y);
+
+    private:
+        int* m_map;
+        std::vector<SDL_Surface*> m_walls;
+        unsigned int m_width;
+        unsigned int m_height;
+};
+
+#endif
