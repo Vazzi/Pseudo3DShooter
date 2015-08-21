809a6610862fe0d5ba996d7e51e9b3dfa792cabb
Jakub Vlasak <vlasakjakub@gmail.com>
2015-08-21 12:59:47 +0200

    Heart anim speed

--- src/objects/Heart.cpp
+++ src/objects/Heart.cpp
@@ -5,10 +5,13 @@ void Heart::update(unsigned int deltaTime) {
     int health = ThePlayer::Instance()->getHealth();
     if (health > 70) {
         m_currentRow = 0;
+        m_animSpeed = 50;
     } else if (health > 30) {
         m_currentRow = 1;
+        m_animSpeed = 30;
     } else {
         m_currentRow = 2;
+        m_animSpeed = 20;
     }
 
     AnimatedObject::update(deltaTime);
